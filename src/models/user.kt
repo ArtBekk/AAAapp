9c533204a158312c476eb537904985208b9c9936
ArtBekk <arturbekk@gmail.com>
2020-10-23 19:16:22 +0700

    Added function that checks user's access to provided resource [Roadmap 3, R1.3]

--- src/models/user.kt
+++ src/models/user.kt
@@ -1,3 +1,32 @@
 package models
 
-class User(val login: String, val password: Int, val write: Array<Resource>?, val read: Array<Resource>?, val execute: Array<Resource>?)
+import Handler
+
+class User(val login: String, val password: Int,
+           private val write: Array<Resource>?,
+           private val read: Array<Resource>?,
+           private val execute: Array<Resource>?) {
+
+    fun hasAccess(handler: Handler): Boolean {
+        return when (handler.role) {
+            "WRITE" -> {
+                write.contains(handler.res)
+            }
+            "READ" -> {
+                read.contains(handler.res)
+            }
+            "EXECUTE" -> {
+                execute.contains(handler.res)
+            }
+            else -> false
+        }
+    }
+
+    fun Array<Resource>?.contains(input: String): Boolean {
+        this?.forEach { x ->
+            return x.contains(input)
+        }
+        return false
+    }
+
+}
