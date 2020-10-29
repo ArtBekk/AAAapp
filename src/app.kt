dd4d59bd6bcb305eef974c9a3ec2336e2cae9fb1
ArtBekk <arturbekk@gmail.com>
2020-10-29 20:20:19 +0700

    Added authorization and accounting steps [Roadmap 10, R1.11]

--- src/app.kt
+++ src/app.kt
@@ -1,5 +1,29 @@
-class App(val args: Array<String>) {
-    fun run() {
+import services.*
+import kotlin.system.exitProcess
+
+class App() {
+    fun run(args: Array<String>): ExitCode {
         val handler = Handler(args)
+        var result: ExitCode
+        /*
+        Checks login and password parameters and authenticates
+         */
+        if (handler.login != "") {
+            if (handler.password != "")
+                result = services.authenticate(handler)
+            else
+                result = ExitCode.WRONGPASSWORD
+        } else result = ExitCode.UNKNOWNLOGIN
+        /*
+        Authorization
+        */
+        if (handler.res != "" && result == ExitCode.SUCCESS)
+            result = authorization(handler)
+        /*
+        Accounting
+        */
+        if (handler.vol != "" && result == ExitCode.SUCCESS)
+            result = accounting(handler)
+        return result
     }
 }
