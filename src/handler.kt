65042294846d18030eb22c9c1494fdb1e3c29a43
AbBlDz <Thetemawild8343743>
2020-10-29 14:58:08 +0700

    Update a file handler [Roadmap 4, R1.8]

--- src/handler.kt
+++ src/handler.kt
@@ -1,25 +1,7 @@
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+
 class Handler(args: Array<String>) {
-
-    init {
-        try {
-            args.forEach {
-                when (it) {
-                    "-login" -> login = args[args.indexOf(it) + 1]
-                    "-pass" -> password = args[args.indexOf(it) + 1]
-                    "-res" -> res = args[args.indexOf(it) + 1]
-                    "-role" -> role = args[args.indexOf(it) + 1]
-                    "-ds" -> ds = args[args.indexOf(it) + 1]
-                    "-de" -> de = args[args.indexOf(it) + 1]
-                    "-vol" -> vol = args[args.indexOf(it) + 1]
-                    "-h" -> helpNeeded = true
-
-                }
-            }
-        } catch (e: IndexOutOfBoundsException) {
-        }
-
-
-    }
 
     var login: String = ""
     var password: String = ""
@@ -28,5 +10,31 @@ class Handler(args: Array<String>) {
     var ds: String = ""
     var de: String = ""
     var vol: String = ""
-    var helpNeeded: Boolean = false
+
+
+    init {
+
+        try {
+            val parser = ArgParser("handler")
+
+            login = parser.option(ArgType.String, shortName = "login",
+                    description = "User login transfer").toString()
+            password = parser.option(ArgType.String, shortName = "pass",
+                    description = "User password transfer").toString()
+            res = parser.option(ArgType.String, shortName = "res",
+                    description = "Transfer of resources").toString()
+            role = parser.option(ArgType.String, shortName = "role",
+                    description = "Specifying the user role").toString()
+            ds = parser.option(ArgType.String, shortName = "ds",
+                    description = "User start date").toString()
+            de = parser.option(ArgType.String, shortName = "de",
+                    description = "User end date").toString()
+            vol = parser.option(ArgType.String, shortName = "vol",
+                    description = "Entering the data size").toString()
+
+            parser.parse(args)
+        } catch (exp: IllegalStateException) {
+        }
+
+    }
 }
