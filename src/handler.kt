c8f98fa8e206e4a4d03aaa795dd7181464e69c6a
ArtBekk <arturbekk@gmail.com>
2020-10-31 17:56:33 +0700

    Fixed parser usage (now handler fields actually store parameters)

--- src/handler.kt
+++ src/handler.kt
@@ -3,13 +3,13 @@ import kotlinx.cli.ArgType
 
 class Handler(args: Array<String>) {
 
-    var login: String = ""
-    var password: String = ""
-    var res: String = ""
-    var role: String = ""
-    var ds: String = ""
-    var de: String = ""
-    var vol: String = ""
+    var login: String? = ""
+    var password: String? = ""
+    var res: String? = ""
+    var role: String? = ""
+    var ds: String? = ""
+    var de: String? = ""
+    var vol: String? = ""
 
 
     init {
@@ -18,19 +18,19 @@ class Handler(args: Array<String>) {
             val parser = ArgParser("handler")
 
             login = parser.option(ArgType.String, shortName = "login",
-                    description = "User login transfer").toString()
+                    description = "User login transfer").value
             password = parser.option(ArgType.String, shortName = "pass",
-                    description = "User password transfer").toString()
+                    description = "User password transfer").value
             res = parser.option(ArgType.String, shortName = "res",
-                    description = "Transfer of resources").toString()
+                    description = "Transfer of resources").value
             role = parser.option(ArgType.String, shortName = "role",
-                    description = "Specifying the user role").toString()
+                    description = "Specifying the user role").value
             ds = parser.option(ArgType.String, shortName = "ds",
-                    description = "User start date").toString()
+                    description = "User start date").value
             de = parser.option(ArgType.String, shortName = "de",
-                    description = "User end date").toString()
+                    description = "User end date").value
             vol = parser.option(ArgType.String, shortName = "vol",
-                    description = "Entering the data size").toString()
+                    description = "Entering the data size").value
 
             parser.parse(args)
         } catch (exp: IllegalStateException) {
