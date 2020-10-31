79007cc42101da0dad959fc8f83eb3cf245a0e47
AbBlDz <Thetemawild8343743>
2020-10-31 20:30:46 +0700

    Changed if statement for authorization[Roadmap 10, R1.1]

--- src/app.kt
+++ src/app.kt
@@ -6,7 +6,7 @@ class App() {
         var result: ExitCode = ExitCode.PRINTHELP
         if (handler.login != null && handler.password != null) {
             result = authenticate(handler)
-            if (handler.res != null && result == ExitCode.SUCCESS) {
+            if (handler.res != null && handler.role != null && result == ExitCode.SUCCESS) {
                 result = authorize(handler)
                 if (handler.vol != null && result == ExitCode.SUCCESS)
                     result = account(handler)
