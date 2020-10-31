5f49969869e650ab4705e67928df11c3ce13cc38
ArtBekk <arturbekk@gmail.com>
2020-10-31 20:06:07 +0700

    Updated app algorithms

--- src/app.kt
+++ src/app.kt
@@ -3,26 +3,15 @@ import services.*
 class App() {
     fun run(args: Array<String>): ExitCode {
         val handler = Handler(args)
-        var result: ExitCode
-        /*
-        Checks login and password parameters and authenticates
-         */
-        result = if (handler.login != "") {
-            if (handler.password != "")
-                authenticate(handler)
-            else
-                ExitCode.WRONGPASSWORD
-        } else ExitCode.UNKNOWNLOGIN
-        /*
-        Authorization
-        */
-        if (handler.res != "" && result == ExitCode.SUCCESS)
-            result = authorize(handler)
-        /*
-        Accounting
-        */
-        if (handler.vol != "" && result == ExitCode.SUCCESS)
-            result = account(handler)
+        var result: ExitCode = ExitCode.PRINTHELP
+        if (handler.login != null && handler.password != null) {
+            result = authenticate(handler)
+            if (handler.res != null && result == ExitCode.SUCCESS) {
+                result = authorize(handler)
+                if (handler.vol != null && result == ExitCode.SUCCESS)
+                    result = account(handler)
+            }
+        }
         return result
     }
 }
