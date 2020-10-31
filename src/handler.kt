6a52d4a3ab79fa4a048823bbbd1ddf43a77fbca2
ArtBekk <arturbekk@gmail.com>
2020-10-31 18:44:28 +0700

    Fixed kotlinx-cli usage

--- src/handler.kt
+++ src/handler.kt
@@ -1,40 +1,25 @@
-import kotlinx.cli.ArgParser
-import kotlinx.cli.ArgType
+import kotlinx.cli.*
 
 class Handler(args: Array<String>) {
 
-    var login: String? = null
-    var password: String? = null
-    var res: String? = null
-    var role: String? = null
-    var ds: String? = null
-    var de: String? = null
-    var vol: String? = null
+    private val parser = ArgParser("handler")
 
+    var login: String? by parser.option(ArgType.String, shortName = "login",
+            description = "User login transfer")
+    var password: String? by parser.option(ArgType.String, shortName = "pass",
+            description = "User password transfer")
+    var res: String? by parser.option(ArgType.String, shortName = "res",
+            description = "Transfer of resources")
+    var role: String? by parser.option(ArgType.String, shortName = "role",
+            description = "Specifying the user role")
+    var ds: String? by parser.option(ArgType.String, shortName = "ds",
+            description = "User start date")
+    var de: String? by parser.option(ArgType.String, shortName = "de",
+            description = "User end date")
+    var vol: String? by parser.option(ArgType.String, shortName = "vol",
+            description = "Entering the data size")
 
     init {
-
-        try {
-            val parser = ArgParser("handler")
-
-            login = parser.option(ArgType.String, shortName = "login",
-                    description = "User login transfer").value
-            password = parser.option(ArgType.String, shortName = "pass",
-                    description = "User password transfer").value
-            res = parser.option(ArgType.String, shortName = "res",
-                    description = "Transfer of resources").value
-            role = parser.option(ArgType.String, shortName = "role",
-                    description = "Specifying the user role").value
-            ds = parser.option(ArgType.String, shortName = "ds",
-                    description = "User start date").value
-            de = parser.option(ArgType.String, shortName = "de",
-                    description = "User end date").value
-            vol = parser.option(ArgType.String, shortName = "vol",
-                    description = "Entering the data size").value
-
-            parser.parse(args)
-        } catch (exp: IllegalStateException) {
-        }
-
+        parser.parse(args)
     }
 }
