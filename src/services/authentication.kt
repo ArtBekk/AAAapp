288e57dc0c1d915200b45a21a0a007473abae9a9
ArtBekk <arturbekk@gmail.com>
2020-10-31 18:00:22 +0700

    code style improvements

--- src/services/authentication.kt
+++ src/services/authentication.kt
@@ -5,12 +5,12 @@ import Handler
 import domains.Users
 
 fun authenticate(handler: Handler): ExitCode {
-    if (handler.login.contains(Regex("[^a-zA-Z0-9]")))
-        return ExitCode.INCORRECTLOGINFORMAT
 
+    if (handler.login!!.matches(".+[a-zA-Z0-9]".toRegex()))
+        return ExitCode.INCORRECTLOGINFORMAT
     Users.forEach {
         if (it.login == handler.login) {
-            return if (it.password == hashSalt(handler.password))
+            return if (it.password == hashSalt(handler.password!!))
                 ExitCode.SUCCESS
             else
                 ExitCode.WRONGPASSWORD
