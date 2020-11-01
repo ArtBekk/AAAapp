7b29150e111909d5937a1160abe0b6ff13a69018
ArtBekk <arturbekk@gmail.com>
2020-11-01 12:16:58 +0700

    Added another user

--- src/domains/users.kt
+++ src/domains/users.kt
@@ -7,9 +7,14 @@ val Users: Array<User> = arrayOf(
         User("ArtBekk", "e858c98a18d35a86c7c7206dc13ba9cb6643f6a4223d8ea8d05fda2ef7da5023",
                 arrayOf(Resource("AA"), Resource("AD.BD"), Resource("AC.BAE")),
                 arrayOf(Resource("AV"), Resource("BB"), Resource("CD.E")),
-                arrayOf(Resource("GG"))),
+                arrayOf(Resource("GG.WP"))),
 
         User("AdamHiggs", "89cd4d1f29716d8262789a7444320538ee9b401b6384a9c92351046edbd9ebf5",
                 arrayOf(Resource("AA"), Resource("AD.BD"), Resource("AC.NAJ")),
                 arrayOf(Resource("AH"), Resource("BB"), Resource("LR.E")),
-                arrayOf(Resource("ZB"))))
+                arrayOf(Resource("ZB"))),
+
+        User("user", "de5d44500b6d1649d10165845e2df21ba323aa94e9f157ca970a03d87c100b48",
+                null,
+                arrayOf(Resource("AC"), Resource("B.EX.DFADS"), Resource("CD.E")),
+                arrayOf(Resource("GL.HF"))))
