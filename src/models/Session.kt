56e43fbacd4dcce223acbb9f5393f60d290156a5
AdamDz <71586194+AbBlDz@users.noreply.github.com>
2020-11-02 20:12:27 +0700

    #1 2. Changed the file name

---
+++ src/models/Session.kt
@@ -0,0 +1,5 @@
+package models
+
+import java.util.Date
+
+class Session (val user: String, val dateStart: Date, val dateEnd: Date, val dataSize: Int)
--- src/models/session.kt
+++
@@ -1,5 +0,0 @@
-package models
-
-import java.util.Date
-
-class Session (val user: String, val dateStart: Date, val dateEnd: Date, val dataSize: Int)
