92a7d8fef6bb4285e5da8565343b8330f7489639
ArtBekk <arturbekk@gmail.com>
2020-11-06 19:46:18 +0700

    #1 9. and 10. Changed array to list and named parameters of the same type

--- src/domains/Users.kt
+++ src/domains/Users.kt
@@ -3,18 +3,18 @@ package domains
 import models.Resource
 import models.User
 
-val Users: Array<User> = arrayOf(
+val Users: List<User> = listOf(
         User("ArtBekk", "e858c98a18d35a86c7c7206dc13ba9cb6643f6a4223d8ea8d05fda2ef7da5023",
-                arrayOf(Resource("AA"), Resource("AD.BD"), Resource("AC.BAE")),
-                arrayOf(Resource("AV"), Resource("BB"), Resource("CD.E")),
-                arrayOf(Resource("GG.WP"))),
+                write = listOf(Resource("AA"), Resource("AD.BD"), Resource("AC.BAE")),
+                read = listOf(Resource("AV"), Resource("BB"), Resource("CD.E")),
+                execute = listOf(Resource("GG.WP"))),
 
         User("AdamHiggs", "89cd4d1f29716d8262789a7444320538ee9b401b6384a9c92351046edbd9ebf5",
-                arrayOf(Resource("AA"), Resource("AD.BD"), Resource("AC.NAJ")),
-                arrayOf(Resource("AH"), Resource("BB"), Resource("LR.E")),
-                arrayOf(Resource("ZB"))),
+                write = listOf(Resource("AA"), Resource("AD.BD"), Resource("AC.NAJ")),
+                read = listOf(Resource("AH"), Resource("BB"), Resource("LR.E")),
+                execute = listOf(Resource("ZB"))),
 
         User("user", "de5d44500b6d1649d10165845e2df21ba323aa94e9f157ca970a03d87c100b48",
-                arrayOf(),
-                arrayOf(Resource("AC"), Resource("B.EX.DFADS"), Resource("CD.E")),
-                arrayOf(Resource("GL.HF"))))
+                write = listOf(),
+                read = listOf(Resource("AC"), Resource("B.EX.DFADS"), Resource("CD.E")),
+                execute = listOf(Resource("GL.HF"))))
