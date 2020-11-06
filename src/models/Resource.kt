eda88e018780d1cc39c528f07f221e467920c51f
ArtBekk <arturbekk@gmail.com>
2020-11-06 21:15:59 +0700

    #1 7. Added description for the function

--- src/models/Resource.kt
+++ src/models/Resource.kt
@@ -4,6 +4,9 @@ class Resource(input: String) {
 
     private val name: List<String> = input.split('.')
 
+    /*
+    * Function splits given string in different parts and compares those with parts of resource's name. Delimiter is '.'
+    * */
     fun contains(input: String): Boolean {
         var result = true
         val splitInput = input.split('.')
