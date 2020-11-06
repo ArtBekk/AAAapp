5b6c2b0f21e91d59c34266c22442dda68516233a
ArtBekk <arturbekk@gmail.com>
2020-11-06 19:49:37 +0700

    Naming

--- src/ExitCodes.kt
+++ src/ExitCodes.kt
@@ -1,10 +1,10 @@
 enum class ExitCode(val value: Int) {
-    SUCCESS(0),
-    PRINTHELP(1),
-    INCORRECTLOGINFORMAT(2),
-    UNKNOWNLOGIN(3),
-    WRONGPASSWORD(4),
-    UNKNOWNROLE(5),
-    NOACCESS(6),
-    INCORRECTACTIVITY(7)
+    Success(0),
+    PrintHelp(1),
+    IncorrectLoginFormat(2),
+    UnknownLogin(3),
+    WrongPassword(4),
+    UnknownRole(5),
+    NoAccess(6),
+    IncorrectActivity(7)
 }
