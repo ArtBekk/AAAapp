6121edc3c19a6f282b98051ff83d64695b47fd96
ArtBekk <arturbekk@gmail.com>
2020-11-06 20:19:08 +0700

    Included classpath for linux/mac

--- RUN.sh
+++ RUN.sh
@@ -1,1 +1,7 @@
-java -cp "./bin/AAAapp.jar;./lib/kotlinx-cli-0.2.1.jar" MainKt $@
+if [[ "$OSTYPE" == "msys" ]]; then
+  params="./bin/AAAapp.jar;./lib/kotlinx-cli-0.2.1.jar"
+else
+  params="./bin/AAAapp.jar:./lib/kotlinx-cli-0.2.1.jar"
+fi
+
+java -cp $params MainKt $@
