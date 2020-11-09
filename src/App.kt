d9e3b89ca0afbc170ab599360ca6dce50b990221
ArtBekk <arturbekk@gmail.com>
2020-11-09 17:00:02 +0700

    #2 5. Large conditions are stored in functions now + refactoring

--- src/App.kt
+++ src/App.kt
@@ -1,18 +1,24 @@
 import services.*
 
-class App() {
-    fun run(args: Array<String>): ExitCode {
-        var result: ExitCode = ExitCode.Success
-        val handler: Handler = if (args.isNullOrEmpty())
-            Handler(arrayOf("-h"))
-        else
-            Handler(args)
-        if (handler.login != null && handler.password != null)
+class App(args: Array<String>) {
+    private val handler: Handler = if (args.isNullOrEmpty())
+        Handler(arrayOf("-h"))
+    else
+        Handler(args)
+
+    private var result: ExitCode = ExitCode.Success
+
+    private fun authenticationNeeded(): Boolean = handler.login != null && handler.password != null
+    private fun authorizationNeeded(): Boolean = authenticationNeeded() && handler.res != null && handler.role != null && result == ExitCode.Success
+    private fun accountingNeeded(): Boolean = authorizationNeeded() && handler.ds != null && handler.de != null && handler.vol != null && result == ExitCode.Success
+
+    fun run(): Int {
+        if (authenticationNeeded())
             result = authenticate(handler)
-        if (handler.res != null && handler.role != null && result == ExitCode.Success)
+        if (authorizationNeeded())
             result = authorize(handler)
-        if (handler.ds != null && handler.de != null && handler.vol != null && result == ExitCode.Success)
+        if (accountingNeeded())
             result = account(handler)
-        return result
+        return result.value
     }
 }
