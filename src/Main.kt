a14b467f00aebb16ac3ba64345d062ce6b38fd70
ArtBekk <arturbekk@gmail.com>
2020-11-16 20:34:58 +0700

    Added initialization of logger

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,9 @@
 import kotlin.system.exitProcess
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
 fun main(args: Array<String>) {
+    val logger: Logger = LogManager.getLogger()
     val app = App(args)
     exitProcess(app.run())
 }
