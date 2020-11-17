95c76eaa4c3bd98904c5f7470c6b8174522f7d6e
ArtBekk <arturbekk@gmail.com>
2020-11-17 19:30:35 +0700

    Added new libraries to scripts and fixed classpath issue

--- BUILD.sh
+++ BUILD.sh
@@ -1,7 +1,8 @@
 if [[ "$OSTYPE" == "msys" ]]; then
-  params="./lib/log4j-api-2.14.0;./lib/log4j-core-2.14.0;./lib/kotlinx-cli-0.2.1.jar"
+  params="./lib/log4j-api-2.14.0.jar;./lib/kotlinx-cli-0.2.1.jar"
 else
-  params="./lib/log4j-api-2.14.0:./lib/log4j-core-2.14.0:./lib/kotlinx-cli-0.2.1.jar"
+  params="./lib/log4j-api-2.14.0.jar:./lib/kotlinx-cli-0.2.1.jar"
 fi
 mkdir -p bin
-kotlinc -include-runtime src/ -d bin/AAAapp.jar -cp $params
+kotlinc-jvm -include-runtime -d bin/app.jar -cp $params src/
+jar uf bin/app.jar -C src log4j2.xml
