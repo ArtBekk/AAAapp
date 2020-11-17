6723d0f5d5dba95e6ef3202691e9193e2d15ff98
ArtBekk <arturbekk@gmail.com>
2020-11-17 14:40:08 +0700

    Added logger libraries to shell scripts

--- BUILD.sh
+++ BUILD.sh
@@ -1,2 +1,7 @@
+if [[ "$OSTYPE" == "msys" ]]; then
+  params="./lib/log4j-api-2.14.0;./lib/log4j-core-2.14.0;./lib/kotlinx-cli-0.2.1.jar"
+else
+  params="./lib/log4j-api-2.14.0:./lib/log4j-core-2.14.0:./lib/kotlinx-cli-0.2.1.jar"
+fi
 mkdir -p bin
-kotlinc -include-runtime src/ -d bin/AAAapp.jar -cp ./lib/kotlinx-cli-0.2.1.jar
+kotlinc -include-runtime src/ -d bin/AAAapp.jar -cp $params
--- RUN.sh
+++ RUN.sh
@@ -1,7 +1,7 @@
 if [[ "$OSTYPE" == "msys" ]]; then
-  params="./bin/AAAapp.jar;./lib/kotlinx-cli-0.2.1.jar"
+  params="./bin/AAAapp.jar;./lib/log4j-api-2.14.0;./lib/log4j-core-2.14.0;./lib/kotlinx-cli-0.2.1.jar"
 else
-  params="./bin/AAAapp.jar:./lib/kotlinx-cli-0.2.1.jar"
+  params="./bin/AAAapp.jar:./lib/log4j-api-2.14.0:./lib/log4j-core-2.14.0:./lib/kotlinx-cli-0.2.1.jar"
 fi
 
 java -cp $params MainKt $@
