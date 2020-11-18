faae74acadfbb5a16a4795d747ab990be9fe1d52
ArtBekk <arturbekk@gmail.com>
2020-11-18 19:53:14 +0700

    Added flyway and h2db to scripts

--- BUILD.sh
+++ BUILD.sh
@@ -1,7 +1,7 @@
 if [[ "$OSTYPE" == "msys" ]]; then
-  params="./lib/log4j-api-2.14.0.jar;./lib/log4j-core-2.14.0.jar;./lib/kotlinx-cli-0.2.1.jar"
+  params="./lib/log4j-api-2.14.0.jar;./lib/log4j-core-2.14.0.jar;./lib/kotlinx-cli-0.2.1.jar;./lib/h2-1.4.200.jar;./lib/flyway-core-7.2.0.jar;."
 else
-  params="./lib/log4j-api-2.14.0.jar:./lib/log4j-core-2.14.0.jar:./lib/kotlinx-cli-0.2.1.jar"
+  params="./lib/log4j-api-2.14.0.jar:./lib/log4j-core-2.14.0.jar:./lib/kotlinx-cli-0.2.1.jar:../lib/h2-1.4.200.jar:./lib/flyway-core-7.2.0.jar:."
 fi
 mkdir -p bin
 kotlinc-jvm -include-runtime -d bin/AAAapp.jar -cp $params src/
--- RUN.sh
+++ RUN.sh
@@ -1,7 +1,7 @@
 if [[ "$OSTYPE" == "msys" ]]; then
-  params="./bin/AAAapp.jar;./lib/log4j-api-2.14.0.jar;./lib/log4j-core-2.14.0.jar;./lib/kotlinx-cli-0.2.1.jar;."
+  params="./bin/AAAapp.jar;./lib/log4j-api-2.14.0.jar;./lib/log4j-core-2.14.0.jar;./lib/kotlinx-cli-0.2.1.jar;./lib/h2-1.4.200.jar;./lib/flyway-core-7.2.0.jar;."
 else
-  params="./bin/AAAapp.jar:./lib/log4j-api-2.14.0.jar:./lib/log4j-core-2.14.0.jar:./lib/kotlinx-cli-0.2.1.jar:."
+  params="./bin/AAAapp.jar:./lib/log4j-api-2.14.0.jar:./lib/log4j-core-2.14.0.jar:./lib/kotlinx-cli-0.2.1.jar:./lib/h2-1.4.200.jar:./lib/flyway-core-7.2.0.jar:."
 fi
 
 java -cp $params MainKt $@
