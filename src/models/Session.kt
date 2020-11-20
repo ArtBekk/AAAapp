c31a055e3cf0d7f14c708029a88a94a1a3557651
ArtBekk <arturbekk@gmail.com>
2020-11-20 19:05:10 +0700

    [Roadmap 6] Changed this model for convenience

--- src/models/Session.kt
+++ src/models/Session.kt
@@ -2,4 +2,4 @@ package models
 
 import java.util.Date
 
-class Session(val user: String, val res: String, val role: Roles, val dateStart: Date, val dateEnd: Date, val dataSize: Int)
+class Session(val user: String, val res: String, val role: String, val dateStart: Date, val dateEnd: Date, val dataSize: Int)
