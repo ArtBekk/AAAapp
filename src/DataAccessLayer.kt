1484e150bc32501fbe81a2e61883eb8472964d39
AbBlDz <Thetemawild8343743>
2020-11-23 22:11:06 +0700

    #3 5. Fixed the errors related to use

--- src/DataAccessLayer.kt
+++ src/DataAccessLayer.kt
@@ -1,80 +1,101 @@
 import models.User
+import java.sql.Connection
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
-import java.sql.Connection
-
 
 class DataAccessLayer(private val connection: Connection) {
 
     val logger: Logger = LogManager.getLogger()
 
     fun userExists(login: String): Boolean {
-        var userExists = false
 
-        logger.info("Preparing and sending an SQL query.")
-        connection.prepareStatement("SELECT* FROM users WHERE login = ?").use {
+        try {
+            var userExists = false
+
+            logger.info("Preparing and sending an SQL query.")
+            val searchUser = connection.prepareStatement("SELECT* FROM users WHERE login = ?")
 
             logger.info("Getting data from a database.")
-            it.setString(1, login)
-            val queryResult = it.executeQuery()
+            searchUser.setString(1, login)
+            val queryResult = searchUser.executeQuery()
 
             logger.info("The data was successfully received.")
             logger.info("Search for the right user.")
             if (queryResult.next()) userExists = true
-        }
+            queryResult.close()
 
-        return userExists
+            return userExists
+        } catch (ex: Exception) {
+
+            logger.info("SQL query failed")
+            return false
+        }
     }
 
     fun getUser(login: String): User {
-        logger.info("Preparing and sending an SQL query.")
-        connection.prepareStatement("SELECT hash, salt FROM users WHERE login = ?").use {
-            it.setString(1, login)
+
+        try {
+            logger.info("Preparing and sending an SQL query.")
+            val getUser = connection.prepareStatement("SELECT hash, salt FROM users WHERE login = ?")
+            getUser.setString(1, login)
 
             logger.info("Getting data from a database.")
-            val result = it.executeQuery()
+            val result = getUser.executeQuery()
             result.next()
             val hash = result.getString("hash")
             val salt = result.getString("salt")
+            result.close()
 
             logger.info("The data was successfully received.")
             return User(login, hash, salt)
+        } catch (ex: Exception) {
+            logger.info("SQL query failed")
+            return User("", "","")
         }
     }
 
     fun getUserAccessInfo(login: String, role: String): MutableList<String> {
-        logger.info("Preparing and sending an SQL query.")
-        connection.prepareStatement("SELECT resource_name FROM resources WHERE login = ? AND role = ?").use {
-            it.setString(1, login)
-            it.setString(2, role)
 
+        try {
+            logger.info("Preparing and sending an SQL query.")
+            val searchRights = connection.prepareStatement("SELECT resource_name FROM resources WHERE login = ? AND role = ?")
 
+            searchRights.setString(1, login)
+            searchRights.setString(2, role)
             logger.info("Getting data from a database.")
-            val result = it.executeQuery()
 
+            val result = searchRights.executeQuery()
             val mutableList = mutableListOf<String>()
             while (result.next())
                 mutableList.add(result.getString("resource_name"))
-
+            result.close()
 
             logger.info("The data was successfully received.")
             return mutableList
+        } catch (ex: Exception) {
+            logger.info("SQL query failed")
+            return mutableListOf()
         }
     }
 
     fun addSession(login: String, role: String, res: String, ds: String, de: String, vol: String) {
-        logger.info("Preparing and sending an SQL query.")
-        connection.prepareStatement("INSERT INTO sessions (login, role, resources, date_start, date_end, data_size) VALUES (?, ?, ?, ?, ?, ?)").use {
 
-            it.setString(1, login)
-            it.setString(2, role)
-            it.setString(3, res)
-            it.setString(4, ds)
-            it.setString(5, de)
-            it.setString(6, vol)
+        try {
+            logger.info("Preparing and sending an SQL query.")
+            val dataSession = connection.prepareStatement("INSERT INTO sessions (login, role, resources, date_start, date_end, data_size) VALUES (?, ?, ?, ?, ?, ?)")
 
-            it.execute()
+            dataSession.setString(1, login)
+            dataSession.setString(2, role)
+            dataSession.setString(3, res)
+            dataSession.setString(4, ds.toString())
+            dataSession.setString(5, de.toString())
+            dataSession.setString(6, vol)
+
+            dataSession.execute()
+            dataSession.close()
             logger.info("The recording session is successfully created.")
+        } catch (ex: Exception) {
+            logger.info("SQL query failed")
         }
     }
 
