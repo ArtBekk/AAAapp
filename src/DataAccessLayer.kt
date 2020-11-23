c12e619b46f8d1180ec13ce0ea000ab659f354b9
AbBlDz <Thetemawild8343743>
2020-11-23 18:38:27 +0700

    #3 3. Fixed Code style

--- src/DataAccessLayer.kt
+++ src/DataAccessLayer.kt
@@ -6,11 +6,14 @@ class DataAccessLayer(private val connection: Connection) {
 
     fun userExists(login: String): Boolean {
         var userExists = false
+
         logger.info("Preparing and sending an SQL query.")
         val searchUser = connection.prepareStatement("SELECT* FROM users WHERE login = ?")
+
         logger.info("Getting data from a database.")
         searchUser.setString(1, login)
         val queryResult = searchUser.executeQuery()
+
         logger.info("The data was successfully received.")
         logger.info("Search for the right user.")
         if (queryResult.next()) userExists = true
@@ -23,12 +26,14 @@ class DataAccessLayer(private val connection: Connection) {
         logger.info("Preparing and sending an SQL query.")
         val getUser = connection.prepareStatement("SELECT hash, salt FROM users WHERE login = ?")
         getUser.setString(1, login)
+
         logger.info("Getting data from a database.")
         val result = getUser.executeQuery()
         result.next()
         val hash = result.getString("hash")
         val salt = result.getString("salt")
         result.close()
+
         logger.info("The data was successfully received.")
         return User(login, hash, salt)
     }
@@ -38,12 +43,14 @@ class DataAccessLayer(private val connection: Connection) {
         val searchRights = connection.prepareStatement("SELECT resource_name FROM resources WHERE login = ? AND role = ?")
         searchRights.setString(1, login)
         searchRights.setString(2, role)
+
         logger.info("Getting data from a database.")
         val result = searchRights.executeQuery()
         val mutableList = mutableListOf<String>()
         while (result.next())
             mutableList.add(result.getString("resource_name"))
         result.close()
+
         logger.info("The data was successfully received.")
         return mutableList
     }
@@ -51,12 +58,14 @@ class DataAccessLayer(private val connection: Connection) {
     fun addSession(login: String, role: String, res: String, ds: Date, de: Date, vol: String) {
         logger.info("Preparing and sending an SQL query.")
         val dataSession = connection.prepareStatement("INSERT INTO sessions (login, role, resources, date_start, date_end, data_size) VALUES (?, ?, ?, ?, ?, ?)")
+
         dataSession.setString(1, login)
         dataSession.setString(2, role)
         dataSession.setString(3, res)
         dataSession.setString(4, ds.toString())
         dataSession.setString(5, de.toString())
         dataSession.setString(6, vol)
+
         dataSession.execute()
         dataSession.close()
         logger.info("The recording session is successfully created.")
