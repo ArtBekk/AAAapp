36e4e053c6278cf2ab83ebdb75f70b080bc6fe85
ArtBekk <arturbekk@gmail.com>
2020-11-28 20:51:18 +0700

    Revert "#3 5. Fixed the errors related to use"
    This brings "use" back

--- src/main/kotlin/DataAccessLayer.kt
+++ src/main/kotlin/DataAccessLayer.kt
@@ -1,7 +1,8 @@
 import models.User
-import java.sql.Connection
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import java.sql.Connection
+
 
 class DataAccessLayer(private val connection: Connection) {
 
@@ -12,17 +13,17 @@ class DataAccessLayer(private val connection: Connection) {
         try {
             var userExists = false
 
-            logger.info("Preparing and sending an SQL query.")
-            val searchUser = connection.prepareStatement("SELECT* FROM users WHERE login = ?")
+        logger.info("Preparing and sending an SQL query.")
+        connection.prepareStatement("SELECT* FROM users WHERE login = ?").use {
 
             logger.info("Getting data from a database.")
-            searchUser.setString(1, login)
-            val queryResult = searchUser.executeQuery()
+            it.setString(1, login)
+            val queryResult = it.executeQuery()
 
             logger.info("The data was successfully received.")
             logger.info("Search for the right user.")
             if (queryResult.next()) userExists = true
-            queryResult.close()
+        }
 
             return userExists
         } catch (ex: Exception) {
@@ -40,11 +41,10 @@ class DataAccessLayer(private val connection: Connection) {
             getUser.setString(1, login)
 
             logger.info("Getting data from a database.")
-            val result = getUser.executeQuery()
+            val result = it.executeQuery()
             result.next()
             val hash = result.getString("hash")
             val salt = result.getString("salt")
-            result.close()
 
             logger.info("The data was successfully received.")
             return User(login, hash, salt)
@@ -60,15 +60,14 @@ class DataAccessLayer(private val connection: Connection) {
             logger.info("Preparing and sending an SQL query.")
             val searchRights = connection.prepareStatement("SELECT resource_name FROM resources WHERE login = ? AND role = ?")
 
-            searchRights.setString(1, login)
-            searchRights.setString(2, role)
-            logger.info("Getting data from a database.")
 
-            val result = searchRights.executeQuery()
+            logger.info("Getting data from a database.")
+            val result = it.executeQuery()
+
             val mutableList = mutableListOf<String>()
             while (result.next())
                 mutableList.add(result.getString("resource_name"))
-            result.close()
+
 
             logger.info("The data was successfully received.")
             return mutableList
@@ -84,15 +83,14 @@ class DataAccessLayer(private val connection: Connection) {
             logger.info("Preparing and sending an SQL query.")
             val dataSession = connection.prepareStatement("INSERT INTO sessions (login, role, resources, date_start, date_end, data_size) VALUES (?, ?, ?, ?, ?, ?)")
 
-            dataSession.setString(1, login)
-            dataSession.setString(2, role)
-            dataSession.setString(3, res)
-            dataSession.setString(4, ds.toString())
-            dataSession.setString(5, de.toString())
-            dataSession.setString(6, vol)
+            it.setString(1, login)
+            it.setString(2, role)
+            it.setString(3, res)
+            it.setString(4, ds)
+            it.setString(5, de)
+            it.setString(6, vol)
 
-            dataSession.execute()
-            dataSession.close()
+            it.execute()
             logger.info("The recording session is successfully created.")
         } catch (ex: Exception) {
             logger.info("SQL query failed")
