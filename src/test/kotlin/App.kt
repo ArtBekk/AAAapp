b88ba1f5b56d614d006f58b2113794ada639929d
ArtBekk <arturbekk@gmail.com>
2020-11-29 13:55:39 +0700

    [Roadmap 4] 5. Created code files for tests

---
+++ src/test/kotlin/App.kt
@@ -0,0 +1,55 @@
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.flywaydb.core.Flyway
+import org.mockito.Mock
+import services.*
+import java.sql.*
+
+class App(args: Array<String>) {
+
+    private val logger: Logger = LogManager.getLogger()
+
+    private val handler: Handler = if (args.isNullOrEmpty())
+        Handler(arrayOf("-h"))
+    else
+        Handler(args)
+
+    @Mock
+    private var dal: DataAccessLayer = DataAccessLayer(connectToDB())
+
+    private var result: ExitCode = ExitCode.Success
+
+    private fun connectToDB(): Connection {
+        val flyway = Flyway.configure().dataSource(System.getenv("URL") + ";MV_STORE=FALSE",
+                System.getenv("LOGIN"),
+                System.getenv("PASS")).locations("filesystem:db/migration").load()
+        flyway.migrate()
+
+        return DriverManager.getConnection(System.getenv("URL") + ";MV_STORE=FALSE",
+                System.getenv("LOGIN"),
+                System.getenv("PASS"))
+    }
+
+    fun run(): Int {
+        if (handler.authenticationNeeded()) {
+            logger.info("STEP 1: Authentication")
+            @Mock
+            result = authenticate(handler, dal)
+            logger.info("Authentication status: $result")
+        }
+        if (handler.authorizationNeeded() && result == ExitCode.Success) {
+            logger.info("STEP 2: Authorization")
+            @Mock
+            result = authorize(handler, dal)
+            logger.info("Authorization status: $result")
+        }
+        if (handler.accountingNeeded() && result == ExitCode.Success) {
+            logger.info("STEP 3: Accounting.")
+            @Mock
+            result = account(handler, dal)
+            logger.info("Accounting status: $result")
+        }
+        dal.closeConnection()
+        return result.value
+    }
+}
